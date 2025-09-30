b86cced7683c287669750fb117aa0badd9393903
aaronspal <aaronspalding59@gmail.com>
2025-09-30 16:33:07 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import RocketTeam from './pages/rocketteam';
-import FormulaSlug from "./pages/formulaslug";
-import ScrollToTop from "./components/scrolltotop/scrolltotop";
-import DataMonkey from "./pages/datamonkey";
-import CMPM from "./pages/cmpm";
-import Music from "./pages/music";
-import Work from "./pages/work";
-import Slugworks from "./pages/slugworks";
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
-        <ScrollToTop/>
         {/*<Home/>*/}
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/rocketteam" element={<RocketTeam/>}/>
-            <Route path="/datamonkey" element={<DataMonkey/>}/>
-            <Route path="/formulaslug" element={<FormulaSlug/>}/>
-            <Route path="/slugworks" element={<Slugworks/>}/>
-            <Route path="/cmpm" element={<CMPM/>}/>
-            <Route path="/work" element={<Work/>}/>
-            <Route path="/music" element={<Music/>}/>
         </Routes>
         {/*<Navigation section="AARON SPALDING"/>*/}
     </div>
---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import './home.css'
+import '../components/libraries/flexgrid.css'
+
+
+import { Link } from 'react-router-dom';
+import Navigation from "../components/navigation/navigation";
+
+function Home() {
+    return (
+        <>
+            testing :D
+        </>
+    );
+}
+
+export default Home;
