e76a5fc1946a60ca757ae892d123197193782e27
aaronspal <aaronspalding59@gmail.com>
2025-09-30 16:36:13 -0700

    testing

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,8 +2,7 @@ import './home.css'
 import '../components/libraries/flexgrid.css'
 
 
-import { Link } from 'react-router-dom';
-import Navigation from "../components/navigation/navigation";
+// import { Link } from 'react-router-dom';
 
 function Home() {
     return (
